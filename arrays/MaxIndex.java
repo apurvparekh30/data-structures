d30c6ffb809b7494f399bbfab8a7e9b2b35a5704
apurvparekh30 <apurvparekh30@gmail.com>
2019-02-13 11:59:20 -0500

    MaxIndex.java

---
+++ arrays/MaxIndex.java
@@ -0,0 +1,90 @@
+
+// Solution to Maxinum Index
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution {
+
+    static int n;
+    static int []arr;
+    static int []minIndex,maxIndex;
+    static FastReader fs = new FastReader();
+
+    public static void main(String[] args) {
+        // code
+        int cases = fs.nextInt();
+        for(int tt=1;tt<=cases;tt++){
+            n = fs.nextInt();
+            arr = new int[n];
+            minIndex = new int[n];
+            maxIndex = new int[n];
+            for(int i=0;i<n;i++)
+                arr[i] = fs.nextInt();
+            minIndex[0] = 0;
+            for(int i=1;i<n;i++)
+                minIndex[i] = arr[i] < arr[minIndex[i-1]] ? i:minIndex[i-1];
+            maxIndex[n-1] = n - 1;
+            for(int i=n-1;i>0;i--)
+                maxIndex[i-1] = arr[i-1] > arr[maxIndex[i]] ? i - 1:maxIndex[i];
+            int i = 0;
+            int j = 0;
+            int max = 0;
+            //System.out.println(Arrays.toString(arr));
+            //System.out.println(Arrays.toString(minIndex));
+            //System.out.println(Arrays.toString(maxIndex));
+            while(i < n && j < n){
+                if(arr[minIndex[i]] <= arr[maxIndex[j]]){
+                    //System.out.println(j-i);
+                    max = (j - i > max) ? j - i : max;
+                    j++;
+                }
+                else 
+                    i++;
+            }
+            System.out.println(max);
+        }
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+    
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+    
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+    
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
