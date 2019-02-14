a3a07da544401df681380f6ee828a3c92c95b71e
apurvparekh30 <apurvparekh30@gmail.com>
2019-02-14 18:31:12 -0500

    MaxIndex.java

--- arrays/MaxIndex.java
+++ arrays/MaxIndex.java
@@ -9,7 +9,7 @@ class Solution {
 
     static int n;
     static int []arr;
-    static int []minIndex,maxIndex;
+    static int []maxIndex;
     static FastReader fs = new FastReader();
 
     public static void main(String[] args) {
@@ -18,25 +18,17 @@ class Solution {
         for(int tt=1;tt<=cases;tt++){
             n = fs.nextInt();
             arr = new int[n];
-            minIndex = new int[n];
             maxIndex = new int[n];
             for(int i=0;i<n;i++)
                 arr[i] = fs.nextInt();
-            minIndex[0] = 0;
-            for(int i=1;i<n;i++)
-                minIndex[i] = arr[i] < arr[minIndex[i-1]] ? i:minIndex[i-1];
             maxIndex[n-1] = n - 1;
             for(int i=n-1;i>0;i--)
                 maxIndex[i-1] = arr[i-1] > arr[maxIndex[i]] ? i - 1:maxIndex[i];
             int i = 0;
             int j = 0;
             int max = 0;
-            //System.out.println(Arrays.toString(arr));
-            //System.out.println(Arrays.toString(minIndex));
-            //System.out.println(Arrays.toString(maxIndex));
             while(i < n && j < n){
-                if(arr[minIndex[i]] <= arr[maxIndex[j]]){
-                    //System.out.println(j-i);
+                if(arr[i] <= arr[maxIndex[j]]){
                     max = (j - i > max) ? j - i : max;
                     j++;
                 }
