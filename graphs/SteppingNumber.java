3ff353da794366f2f55dcec548ecfc5f68b95c7e
apurvparekh30 <apurvparekh30@gmail.com>
2019-03-08 11:52:09 -0500

    SteppingNumber.java

--- graphs/SteppingNumber.java
+++ graphs/SteppingNumber.java
@@ -6,6 +6,7 @@ class Solution {
     static int count = 0;
     static int n,m;
 
+    // string approach
     static void dfs(String number){
         int num = Integer.parseInt(number);
         //System.out.println(num);
@@ -20,6 +21,19 @@ class Solution {
         if(last-1 >= 0)
             dfs(number + (char)(last - 1 + '0'));
     }
+
+    // integer approach
+    static void rec(int num){
+        if(num > m)
+            return;
+        if(num >= n)
+            count++;
+        int last = num % 10;
+        if(last + 1 <= 9)
+            rec(num*10+last+1);
+        if(last - 1 >= 0)
+            rec(num*10+last-1);
+    }
     public static void main(String[] args) {
         int tc = fs.nextInt();
         while(tc-- > 0){
@@ -29,7 +43,7 @@ class Solution {
             if(n==0)
                 count++;
             for(int i=1;i<=9;i++){
-                dfs(""+(char)(i+'0'));
+                rec(i);
             }
             System.out.println(count);
         }
