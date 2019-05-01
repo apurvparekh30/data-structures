f9eaf2ec1c758ecbe94f7ef58ebc42cc3db3dab6
apurvparekh30 <apurvparekh30@gmail.com>
2019-05-01 15:43:30 -0400

    FormPalindrome.java

--- strings/FormPalindrome.java
+++ strings/FormPalindrome.java
@@ -1,72 +1,37 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class GFG {
+class Solution {
+    static Scanner fs = new Scanner(System.in);
+    static char []string;
+    static int [][]dp;
 
-    static FastReader fs = new FastReader();
-    static char []arr;
-    static int memo[][];
     static int rec(int i,int j){
-        if(i>j)
+        if(i>=j)
             return 0;
-        if(i==j)
-            return 0;
-        if(memo[i][j] != -1)
-            return memo[i][j];
-        if(arr[i] == arr[j])
-            return rec(i+1,j-1);
-        return memo[i][j] = 1 + Math.min(rec(i+1,j),rec(i,j-1));
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int ans = Integer.MAX_VALUE;
+        if(string[i]==string[j]){
+            ans = Math.min(ans,rec(i+1,j-1));
+        }
+        else {
+            ans = Math.min(ans,1 + rec(i+1,j));
+            ans = Math.min(ans,1 + rec(i,j-1));
+        }
+        return dp[i][j] = ans;
     }
+
+
     public static void main(String[] args) {
-        int cases = fs.nextInt();
-        for(int tt = 1;tt <= cases; tt++){
-            arr = fs.next().toCharArray();
-            memo = new int[arr.length][arr.length];
-            for(int i=0;i<arr.length;i++)
-                Arrays.fill(memo[i],-1);
-            System.out.println(rec(0,arr.length-1));
-        }
-    }
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-    
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-    
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        int tc = fs.nextInt();
+        while(tc-- > 0) {
+            string = fs.next().toCharArray();
+            int n = string.length;
+            dp = new int[n][n];
+            for(int []r:dp){
+                Arrays.fill(r,-1);
             }
-            return st.nextToken();
-        }
-    
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-    
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-    
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
+            System.out.println(rec(0,n-1));
         }
     }
 }
