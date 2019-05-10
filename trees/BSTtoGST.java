50df3ff3181ccd99a0e1db8f24684a933166b751
apurvparekh30 <apurvparekh30@gmail.com>
2019-05-09 23:45:46 -0400

    BstToGST.java

--- trees/BSTtoGST.java
+++ trees/BSTtoGST.java
@@ -1,37 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     
-    class state {
-        TreeNode node;
-        int ret;
-        state(TreeNode node,int ret){
-            this.node = node;
-            this.ret = ret;
-        }
-    } 
-    
-    public state rec(TreeNode root,int data){
-        if(root==null){
-            return new state(null,0);
-        }
-        System.out.println(root.val + " " + data);
-        state right = rec(root.right,data);
-        TreeNode newRoot = new TreeNode(data + root.val + right.ret);
-        newRoot.right = right.node;
-        state left = rec(root.left,newRoot.val);
-        newRoot.left = left.node;
-        return new state(newRoot,(newRoot.val + (root.left==null ? 0:root.left.val)));
-    }
+    int pre = 0;
     
     public TreeNode bstToGst(TreeNode root) {
-        return rec(root,0).node;
+        if(root.right!=null)
+            bstToGst(root.right);
+        pre = root.val = root.val + pre;
+        if(root.left!=null)
+            bstToGst(root.left);
+        return root;
     }
 }
